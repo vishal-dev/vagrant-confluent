e2a08fc934dee1853a8ff54de4b9bfd60ef04b48
Vishal Kumar Puthalath <vishalkumar.puthalath@avisbudget.com>
2019-05-30 14:44:39 -0400

    update confluent-install file with comments

--- provisioners/confluent-install.sh
+++ provisioners/confluent-install.sh
@@ -1,6 +1,7 @@
+# go to https://www.confluent.io/download/ and download the zip file confluent-5.2.1-2.12.zip to your local machine.
+#make sure that you have the zip file moved to the folder where vagrant file is placed.
 cd /vagrant/
 unzip confluent-5.2.1-2.12.zip
 cd confluent-5.2.1
 export PATH=/vagrant/confluent-5.2.1/bin:$PATH
 confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:latest
-confluent start
