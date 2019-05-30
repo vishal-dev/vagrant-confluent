bce5c911fc00e975856c0d4438dfab46657ebbb6
Vishal Kumar Puthalath <vishalkumar.puthalath@avisbudget.com>
2019-05-30 14:37:40 -0400

    delete unwanted files

--- provisioners/jenkins-install.sh
+++
@@ -1,5 +0,0 @@
-#install jenkins on ubuntu
-sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt update
-sudo apt install jenkins
